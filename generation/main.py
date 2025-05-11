0a70c526ec8a3986f277f0536f4a813b2cfdf4d1
Leo Kling <leo.kling.info@gmail.com>
2025-05-11 17:52:49 +0200

    idk

--- generation/main.py
+++ generation/main.py
@@ -1,8 +1,14 @@
+from treble_tsdk.tsdk import TSDK, TSDKCredentials
 from treble_tsdk import display_data as dd
-from treble_tsdk.tsdk import TSDK, TSDKCredentials, 
+from treble_tsdk import treble
 
 def main():
     tsdk = TSDK(TSDKCredentials.from_file("./creds/tsdk.cred"))
+
+    projects = tsdk.list_my_projects()
+    dd.display(projects)
+
+    project = tsdk.get_or_create_project("ertsi")
 
 
 if __name__ == "__main__":
